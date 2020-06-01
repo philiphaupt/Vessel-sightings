7d745c19cdf69c8add993fccdaffcf3acf3731a9
philiphaupt <philip.haupt@gmail.com>
2020-06-01 18:04:25 +0100

    succesfully converted DMS and converted to decimal degrees - to check the formula to convert minutes-decimal_seconds to decimal seconds

--- scripts/convert_coordinates.R
+++ scripts/convert_coordinates.R
@@ -5,22 +5,38 @@ library(sp)
 library(tidyverse)
 #library(stringr)
 
+# aim convert and concatenate coordinates to decimal degrees: from Deciomal degrees + decimal minutes to decimal degrees
+# method: split minutes decimal seconds into two columns, then multiply the decimal seconds with 60 and concatenate three coliumns to get DMS Then use sp char2DMS to get an offical data type. then convert to numeric to get decimal degrees
+
 # isolate characters
 # Decimal_degrees
-chd = as.character(boat_sightings$Lattitude)
+d_y <-  as.character(boat_sightings$Lattitude)
+d_x <- as.character(boat_sightings$Longitude)
 
-#temporary storage for minutes and decimal seconds
-ddm_tmp <- stringr::str_split_fixed(string = as.character(boat_sightings$Lat_min),
-                         pattern =  "[[:punct:]]",
-                         n = 2)
-#convert each its own vector
-chm = ddm_tmp[,1]
-chs = ddm_tmp[,2]
+# Minutes 
+m_y <- stringr::str_split_fixed(string = as.character(boat_sightings$Lat_min),
+                                                         pattern =  "[[:punct:]]",
+                                                         n = 2)[,1]
+m_x <- stringr::str_split_fixed(string = as.character(boat_sightings$Long_min),
+                                                         pattern =  "[[:punct:]]",
+                                                         n = 2)[,1]
+# Seconds
+s_y <- stringr::str_split_fixed(string = as.character(boat_sightings$Lat_min),
+                                pattern =  "[[:punct:]]",
+                                n = 2)[,2] %>% 
+   as.numeric() *100 / 60 #%>% 
+  # as.character()
 
-# concatenate lat and longs into single columns
-boat_sightings %>% mutate(x_ddms == char2dms("Lattitude",""))
+s_x <- stringr::str_split_fixed(string = as.character(boat_sightings$Long_min),
+                                pattern =  "[[:punct:]]",
+                                n = 2)[,2] %>% 
+  as.numeric() *100 / 60 #%>% 
+  # as.character()
+
+# concatenate into a single character
+dms_y <- paste0(d_y, "°",m_y,"′",as.character(s_y), "″W")
+dms_y_2 <- char2dms(dms_y, chd = "°", chm = "′", chs = "″")
 
 
-data(state)
-dd2dms(state.center$x)
-sp::char2dms()
+#convert to decimal coordinates
+dd_y <- as.numeric(dms_y_2)
