263b2453330e2fa02e51e94785d79fc377e58e76
philiphaupt <philip.haupt@gmail.com>
2020-06-01 17:04:15 +0100

    read in and isolate coordinates into vectors

--- scripts/convert_coordinates.R
+++ scripts/convert_coordinates.R
@@ -3,17 +3,22 @@
 library(sf)
 library(sp)
 library(tidyverse)
+#library(stringr)
 
-#isolate cahracters
-chd = boat_sightings$Lattitude
-chm <- strsplit(x = as.character(boat_sightings$Lat_min),
-         split =  "[[:punct:]]")
-gsub(pattern = "*.", x = boat_sightings$Lat_min)
-chm = substr(boat_sightings$Lat_min, 1, 2)
-chs = substr(boat_sightings$Lat_min, 3, 2)
+# isolate characters
+# Decimal_degrees
+chd = as.character(boat_sightings$Lattitude)
+
+#temporary storage for minutes and decimal seconds
+ddm_tmp <- stringr::str_split_fixed(string = as.character(boat_sightings$Lat_min),
+                         pattern =  "[[:punct:]]",
+                         n = 2)
+#convert each its own vector
+chm = ddm_tmp[,1]
+chs = ddm_tmp[,2]
 
 # concatenate lat and longs into single columns
-boat_sightings %>% mutate(x_ddms == ("Lattitude",""))
+boat_sightings %>% mutate(x_ddms == char2dms("Lattitude",""))
 
 
 data(state)
