65d640fe37c743275cb3a4b68e8cf929bf5d7a61
philiphaupt <philip.haupt@gmail.com>
2020-06-01 18:04:43 +0100

    remove NA and LAttitude missing

--- scripts/read_xl_vessel_sightings.R
+++ scripts/read_xl_vessel_sightings.R
@@ -3,6 +3,9 @@
 library(readxl)
 library(tidyverse)
 
-boat_sightings <- readxl::read_xlsx("C:/Users/Phillip Haupt/OneDrive - Kent & Essex Inshore Fisheries and Conservation Authority/Boat_sightings_20150101_to_20200601.xlsx")
-
-#
+boat_sightings <- readxl::read_xlsx("C:/Users/Phillip Haupt/OneDrive - Kent & Essex Inshore Fisheries and Conservation Authority/Boat_sightings_20150101_to_20200601.xlsx") %>% 
+  filter(!is.na(Lattitude)) %>% 
+  filter(Lattitude != 0) %>% 
+  filter(!is.na(Longitude)) %>% 
+  filter(!is.na(Lat_min)) %>% 
+  filter(!is.na(Long_min))
