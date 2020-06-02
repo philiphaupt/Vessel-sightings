ae05371075f4412692dbc5bd98e12f172ed53933
philiphaupt <philip.haupt@gmail.com>
2020-06-02 16:10:14 +0100

    add saving csv and remove geometry

--- scripts/find_pts_in_ices_rect.R
+++ scripts/find_pts_in_ices_rect.R
@@ -1,3 +1,8 @@
 #intersect_with_ices_rectangles
 
 my_points <- sf::st_intersection(sightings_sf, my_ices_rect_sf)
+vessel_sightings_32f0_32f1 <- my_points
+
+# remove geometry and save it as an excel file
+st_geometry(vessel_sightings_32f0_32f1) <- NULL
+write_excel_csv(vessel_sightings_32f0_32f1, "./vessel_sightings_32f0_32f1.csv")
