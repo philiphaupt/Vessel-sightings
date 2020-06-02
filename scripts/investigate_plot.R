5c02db42727cc5fe8af83f28cf2b45f6c4a46d7d
philiphaupt <philip.haupt@gmail.com>
2020-06-02 13:40:31 +0100

    amend script to plot vessel sightings over the top of ICES rectangles

--- scripts/investigate_plot.R
+++ scripts/investigate_plot.R
@@ -3,6 +3,32 @@ library(tmap)
 
 tmap::tmap_mode("view")
 
-sightings_sf %>% 
+
+# Plot rectangles
+my_ices_rect_sf %>% 
+  tm_shape()+
+  tm_fill(col = "salmon",
+          alpha = 0.5)
+
+tmap::tmap_mode("plot")
+# Plot sightings data
+sightings_sf %>% sample_frac(size = 0.1) %>% #randomly select 10 % of points for test plot
   tm_shape() +
-  tmap::tm_symbols()
+  tmap::tm_symbols(size = 0.6)+
+  my_ices_rect_sf %>% 
+  tm_shape()+
+  tm_fill(col = "salmon",
+          alpha = 0.5)
+
+
+
+# Plot only points inside
+# Plot sightings data
+my_points %>% 
+  tm_shape() +
+  tmap::tm_symbols(size = 0.3)+
+  my_ices_rect_sf %>% 
+  tm_shape()+
+  tm_fill(col = "salmon",
+          alpha = 0.5)
+
