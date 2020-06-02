3c9c6304b75f36d98d91eec5ad99399d72e06cf4
philiphaupt <philip.haupt@gmail.com>
2020-06-02 13:41:26 +0100

    add new script to write gis output file

---
+++ write_gis_file.R
@@ -0,0 +1,3 @@
+# Write to GIS file
+
+sf::write_sf(my_points, dsn = "./vessel_sightings_ices_32F0_32F1_20150101_20200601.gpkg", layer = "vessel_sightings_ices_32F0_32F1_20150101_20200601", driver = "GPKG")
