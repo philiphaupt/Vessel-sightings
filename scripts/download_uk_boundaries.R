8987ab421ebad30ee7af076cf79d90f53609d84b
philiphaupt <philip.haupt@gmail.com>
2020-07-03 14:58:42 +0100

    Add a message at end

--- scripts/download_uk_boundaries.R
+++ scripts/download_uk_boundaries.R
@@ -2,7 +2,7 @@
 library("GADMTools")
 library("tmap")
 
-uk_l2 <- gadm_sf_loadCountries("GBR", level = 3, basefile = "./")
+uk_l3 <- gadm_sf_loadCountries("GBR", level = 3, basefile = "./")
 uk_l2 <- gadm_sf_loadCountries("GBR", level = 2, basefile = "./")
 uk_l1 <- gadm_sf_loadCountries("GBR", level = 1, basefile = "./")
 
@@ -14,4 +14,6 @@ england <- uk_l1$sf %>% filter(NAME_1 == "England")
 #isolate kent and essex
 kent_and_essex <- uk_l2$sf %>% filter(NAME_2 == "Essex"|NAME_2 == "Kent")
 # house keeping
-rm(uk_l1, uk_l2)
+rm(uk_l1, uk_l2, uk_l3)
+
+print("two objects were created: england and kent and essex. England is for clipping (clipping vessel sightings that fall over land) and map making, and kent and essex for mapping only.")
