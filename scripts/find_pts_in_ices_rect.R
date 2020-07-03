166d1560cf706071780094781880f846da91348d
philiphaupt <philip.haupt@gmail.com>
2020-07-03 14:58:48 +0100

    Add a message at end

--- scripts/find_pts_in_ices_rect.R
+++ scripts/find_pts_in_ices_rect.R
@@ -5,4 +5,5 @@ vessel_sightings_32f0_32f1 <- overlap_points
 
 # remove geometry and save it as an excel file
 #st_geometry(vessel_sightings_32f0_32f1) <- NULL
-write_excel_csv(vessel_sightings_32f0_32f1, "./vessel_sightings_32f0_32f1.csv")
+#write_excel_csv(vessel_sightings_32f0_32f1, "./vessel_sightings_32f0_32f1.csv")
+print("Two objects were created: overlap_points and vessel_sightings_32f0_32f1 - at this stage they are identical")
