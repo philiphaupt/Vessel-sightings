715b31b5c9e3377b5035e3835e8c5d413f89b8cb
philiphaupt <philip.haupt@gmail.com>
2020-07-03 15:00:12 +0100

    upload script to make a map of the outputs

---
+++ scripts/output_map_plot.R
@@ -0,0 +1,39 @@
+# investigate plot 2
+
+# Using the plotting function:
+tmap::tm_shape(shp = area_of_interest) +
+  tm_fill(col = NA,
+          alpha = 0) +
+  tm_graticules(lwd = 0.8,
+                col = "grey") +
+  tm_shape(shp = england) +
+  tm_borders("grey") +
+  tm_fill("cornsilk1",
+          alpha =1) +
+  tm_shape(shp = kent_and_essex) +
+  tm_borders("grey60", lwd = 0.5) +
+  tm_text("NAME_2",
+          size = 1.2,
+          col = "black",
+          just = "left") +
+  tm_fill(col = "darkolivegreen3",
+          alpha = 1) +
+  tm_shape(vessel_sightings_32f0_32f1_water) +
+  tm_bubbles(
+    size = 0.1,
+    col = "black",
+    alpha = 1,
+    border.col = "NA",
+    border.lwd = 0
+  ) +
+  tm_layout(bg.color = "lightblue",
+            main.title = "Vessel sightings in ICES 32f0 and 32f1 between 2015 and 2020",
+            main.title.position = "center"
+            #title.position = c("centre")
+            #attr.position = c("TOP", "RIGHT")
+  )+
+  #tm_logo(file = "logo.PNG",
+  #        just = 1)+
+  tm_compass(type = "8star",
+             position = c("right","top"))+
+  tm_scale_bar(position = c("left", "top"))
